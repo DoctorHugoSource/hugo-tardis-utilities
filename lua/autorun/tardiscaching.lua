81c17c7014da7f06fde227957a8c7116b6522278
doctorhugo <doctorhugosource@gmail.com>
2024-08-22 11:51:14 +0200

    turn off caching

--- lua/autorun/tardiscaching.lua
+++ lua/autorun/tardiscaching.lua
@@ -1,7 +1,9 @@
 
-hook.Add( "PlayerInitialSpawn", "vortexcaching", function( ply )
+-- priorly used to avoid the hangup when loading a new vortex via vortex swap, but with the new compressed vortex they load so fast on modern pcs, it may be unecessary
 
-util.PrecacheModel("models/doctorwho1200/toyota/2014timevortex.mdl")
-util.PrecacheModel("models/hugoextension/tuatvortex/tuattimevortex.mdl")
+-- hook.Add( "PlayerInitialSpawn", "vortexcaching", function( ply )
 
-end)
+-- util.PrecacheModel("models/doctorwho1200/toyota/2014timevortex.mdl")
+-- util.PrecacheModel("models/hugoextension/tuatvortex/tuattimevortex.mdl")
+
+-- end)
